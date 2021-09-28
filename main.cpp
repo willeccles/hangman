e08cc7b8ce276c79dd6f8f6217b227e460455bd0
Will Eccles <will@eccles.dev>
2021-09-28 09:47:05 -0400

    main.cpp: some tweaks that fixed nothing

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <csignal>
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 
@@ -8,17 +9,24 @@
 
 namespace {
 
-void AltScreenBuf() { /*std::cout << "\033[?1049h";*/
-}
-void ResetScreenBuf() { std::cout << "\033[?1049l"; }
+void AltScreenBuf() { (std::cout << "\033[?1049h").flush(); }
+void ResetScreenBuf() { (std::cout << "\033[?1049l").flush(); }
 
-void Clear() { std::cout << "\033[2J\033[H"; }
+void Clear() { std::cout << "\033[2J"; }
+
+// Move cursor to position (1-based, so 1,1 is top left!).
+void Cursor(unsigned row, unsigned col) {
+  std::printf("\033[%u;%uH", row, col);
+}
 
 }  // namespace
 
 void SigHandler(int) { std::exit(0); }
 
 int main(int argc, char** argv) {
+  std::ios::sync_with_stdio(true);
+  std::cout << std::unitbuf;
+
   if (argc != 2) {
     std::cerr << "usage: " << argv[0] << " WORDLIST\n";
     return 1;
@@ -42,11 +50,14 @@ int main(int argc, char** argv) {
 
   AltScreenBuf();
   Clear();
+  Cursor(1, 1);
 
   hangman::Man man;
   hangman::ObscuredWord word(list());
 
   std::cout << "Welcome to hangman!\n\n";
+  // TODO clean this up
+  // TODO do not clear after every entry (causes flickering during updates)
   do {
     char c;
 
@@ -59,19 +70,23 @@ int main(int argc, char** argv) {
     switch (word.Guess(c)) {
       case hangman::ObscuredWord::kCorrect:
         Clear();
+        Cursor(1, 1);
         std::cout << "You got it!\n\n";
         break;
       case hangman::ObscuredWord::kIncorrect:
         ++man;
         Clear();
+        Cursor(1, 1);
         std::cout << "Sorry, that's not right :(\n\n";
         break;
       case hangman::ObscuredWord::kRepeat:
         Clear();
+        Cursor(1, 1);
         std::cout << "You already guessed that!\n\n";
         break;
       case hangman::ObscuredWord::kInvalid:
         Clear();
+        Cursor(1, 1);
         std::cout << "Invalid input!\n\n";
         break;
     }
@@ -81,6 +96,7 @@ int main(int argc, char** argv) {
 
   ResetScreenBuf();
 
+  // TODO this will not display?
   if (man.Dead()) {
     std::cout << "You lost!\n\n";
   } else {
